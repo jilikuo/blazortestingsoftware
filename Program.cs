6e9c5ebbc7754bce7a14e4c3630aabcc6f2b98b4
jilikuo <54220633+jilikuo@users.noreply.github.com>
2024-07-11 08:04:35 -0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System.Threading.Tasks;
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var builder = WebAssemblyHostBuilder.CreateDefault(args);
+        builder.RootComponents.Add<App>("#app");
+
+        await builder.Build().RunAsync();
+    }
+}
