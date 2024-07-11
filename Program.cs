fdd2c1a4a4ad9cb292272604d1b8ec7dfa588948
jilikuo <54220633+jilikuo@users.noreply.github.com>
2024-07-11 08:25:32 -0300

    trying to run simple app

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using System.Threading.Tasks;
+using BlazorTestSoftware;
 
 public class Program
 {
