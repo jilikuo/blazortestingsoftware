a6eef4860acd0c78c0ef64472b64fbf29a161085
jilikuo <54220633+jilikuo@users.noreply.github.com>
2024-07-11 15:42:19 -0300

    test

--- Program.cs
+++ Program.cs
@@ -1,17 +1,66 @@
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.DependencyInjection;
-using System.Threading.Tasks;
+#if (Interactivity == "WebAssembly" || Interactivity == "Auto")
+using BlazorMin.Client.Pages;
+#endif
+using BlazorMin.Components;
 
-namespace BlazorTestSoftware
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+#if (Interactivity == "None")
+builder.Services.AddRazorComponents();
+#else
+builder.Services.AddRazorComponents()
+    #if (Interactivity == "Auto")
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+    #elif (Interactivity == "Server")
+    .AddInteractiveServerComponents();
+    #elif (Interactivity == "WebAssembly")
+    .AddInteractiveWebAssemblyComponents();
+    #endif
+#endif
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+#if (Interactivity == "WebAssembly" || Interactivity == "Auto")
+if (app.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("#app");
-
-            await builder.Build().RunAsync();
-        }
-    }
+    app.UseWebAssemblyDebugging();
 }
+else
+#else
+if (!app.Environment.IsDevelopment())
+#endif
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+#if (!NoHttps)
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+#endif
+}
+
+#if (!NoHttps)
+app.UseHttpsRedirection();
+
+#endif
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+#if (Interactivity == "None")
+app.MapRazorComponents<App>();
+#else
+app.MapRazorComponents<App>()
+    #if (Interactivity == "Server")
+    .AddInteractiveServerRenderMode();
+    #endif
+    #if (Interactivity == "Auto")
+    .AddInteractiveServerRenderMode()
+    #endif
+    #if (Interactivity == "WebAssembly" || Interactivity == "Auto")
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(Home).Assembly);
+    #endif
+#endif
+
+app.Run();
